2e5e285c3e5921a4f6f9e7b05e007eefd3d31c89
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-15 20:00:18 +0000

    implement setupInput and handlseUserInput

--- play.js
+++ play.js
@@ -2,26 +2,25 @@ const { connect } = require("./client")
 
 const net = require("net");
 
-// establishes a connection with the game server
-/* const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: "50541",
-  });
+const setupInput = function () {
 
-  conn.on("connect", () => {
-    console.log("Connecting ...");
-  });
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
 
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
 
-    // interpret incoming data as text
-  conn.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
 
-  return conn;
-}; */
+  return stdin;
+
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
