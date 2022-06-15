245084854f77cbeab025594b558b6fddde42480d
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-15 22:34:43 +0000

    implement WASD movement

--- client.js
+++ client.js
@@ -2,43 +2,29 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
+ 
   // connection command
   const conn = net.createConnection({
     host: "10.0.2.15",
     port: "50541",
   });
+  
   // connection stablished
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
   });
+  
   // message from the server
   conn.on("data", (data) => {
     console.log("Server says:", data);
   });
+ 
   // snake initials
   conn.on("connect", () => {
     conn.write("Name: VGB");
   });
-  // move up    
-  conn.on("connect", () => {
-    conn.write("Move: up");
-
-/*     testing with setInterval
-
-    const myInterval = setInterval(() => {
-      conn.write("Move: up");
-    }, 1000);
-
-    myInterval;
-
-    setInterval(() => {
-      clearInterval(myInterval)
-    }, 5000); */
-
-  });
 
   // interpret incoming data as text
-
   conn.setEncoding("utf8");
 
   return conn;
---
+++ input.js
@@ -0,0 +1,35 @@
+let connection;
+
+const setupInput = (conn) => {
+
+  connection = conn;
+
+  // setup command
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // keyboard event handler
+  const handleUserInput = function (key) {
+
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      conn.write("Move: up")
+    } else if (key === 'a') {
+      conn.write("Move: left");
+    } else if (key === 's') {
+      conn.write("Move: down");
+    } else if (key === 'd') {
+      conn.write("Move: right");
+    }
+  }
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+
+};
+
+module.exports = { setupInput };
